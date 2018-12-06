b83cb15266074d943768e9d95e96e67a885942ef
Tinet-wenjd <wenjd@ti-net.com.cn>
2018-12-06 20:13:48 +0800

    begin

--- house/src/main/java/com/air/garden/house/controller/AlarmSettingController.java
+++ house/src/main/java/com/air/garden/house/controller/AlarmSettingController.java
@@ -2,12 +2,15 @@ package com.air.garden.house.controller;
 
 import com.air.garden.house.mapper.AlarmSettingMapper;
 import com.air.garden.house.model.AlarmSetting;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author wenjd
@@ -37,5 +40,14 @@ public class AlarmSettingController {
         alarmSettingMapper.insertSelective(alarmSetting);
         return "ok";
     }
+
+
+    @GetMapping("/alarm")
+    public String getAlarmInfo() {
+        List<AlarmSetting> alarmSettingList = alarmSettingMapper.listByEnterpriseId(8000091);
+        String alarmInfo = JSONObject.toJSONString(alarmSettingList);
+        return alarmInfo;
+    }
+
 
 }
--- house/src/main/java/com/air/garden/house/mapper/AlarmSettingMapper.java
+++ house/src/main/java/com/air/garden/house/mapper/AlarmSettingMapper.java
@@ -2,8 +2,11 @@ package com.air.garden.house.mapper;
 
 import com.air.garden.house.common.BaseMapper;
 import com.air.garden.house.model.AlarmSetting;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * 告警设置项信息
@@ -15,5 +18,6 @@ import org.springframework.stereotype.Component;
 @Component("alarmSettingMapper")
 public interface AlarmSettingMapper extends BaseMapper<AlarmSetting, Integer> {
 
+    List<AlarmSetting> listByEnterpriseId(@Param("enterpriseId") Integer enterpriseId);
 
 }
